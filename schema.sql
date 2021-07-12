747c01568487d7001b82e1100259197c5aa593c1
Mehdi <mf1378mf@yahoo.com>
2021-07-12 16:45:03 +0430

    first table user

--- schema.sql
+++ schema.sql
@@ -1,1 +1,14 @@
-    
+create table user
+(
+	"first name" nvarchar not null,
+	"last name" nvarchar not null,
+	intro nvarchar,
+	birthday nchar not null,
+	id INTEGER not null
+		constraint user_pk
+			primary key autoincrement
+);
+
+create unique index user_id_uindex
+	on user (id);
+
