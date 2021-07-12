817df05824f8717070e6ece773acfaf211b2d21d
Mehdi <mf1378mf@yahoo.com>
2021-07-12 17:36:47 +0430

    see ehsan

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-create table user
+create table if not exists user
 (
 	"first name" nvarchar not null,
 	"last name" nvarchar not null,
@@ -11,4 +11,19 @@ create table user
 
 create unique index user_id_uindex
 	on user (id);
+
+create table if not exists skill
+(
+	user_id INTEGER not null
+		constraint skill_user_id_fk
+			references user
+				on update cascade on delete cascade,
+	"id " INTEGER not null
+		constraint skill_pk
+			primary key autoincrement,
+	text nvarchar not null
+);
+
+create unique index "skill_id _uindex"
+	on skill ("id ");
 
