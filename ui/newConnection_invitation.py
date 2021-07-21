225328bce0f04a34972b18d4ef926f10a9d59cfa
Mehdi <mf1378mf@yahoo.com>
2021-07-21 12:54:21 +0430

    add py

--- ui/newConnection_invitation.py
+++ ui/newConnection_invitation.py
@@ -1,17 +1,16 @@
-import datetime
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'newConnection_invitation.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.1
+#
+# WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QLabel, QVBoxLayout, QWidget, QScrollArea
-
-from .simple import ui as ui_simple
-from tables import User, Connection
-from tables.notification import Notification
 
 
 class Ui_NewConnection(object):
-    def setupUi(self, NewConnection, data):
-        self.data = data
+    def setupUi(self, NewConnection):
         NewConnection.setObjectName("NewConnection")
         NewConnection.resize(600, 600)
         self.centralwidget = QtWidgets.QWidget(NewConnection)
@@ -93,13 +92,9 @@ class Ui_NewConnection(object):
         NewConnection.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(NewConnection)
         self.statusbar.setObjectName("statusbar")
-        self.find_invitations()
         NewConnection.setStatusBar(self.statusbar)
 
         self.retranslateUi(NewConnection)
-        from .network import ui as ui_network
-        self.BackButton.clicked.connect(lambda: ui_network.setupUi(NewConnection,self.data))
-        self.SearchButton.clicked.connect(lambda: self.search())
         QtCore.QMetaObject.connectSlotsByName(NewConnection)
 
     def retranslateUi(self, NewConnection):
@@ -119,114 +114,14 @@ class Ui_NewConnection(object):
         self.ConnnectButton_Yes.setText(_translate("NewConnection", "Yes, Connect"))
         self.BackButton.setText(_translate("NewConnection", "Back"))
 
-    def search(self):
-        username = self.lineEdit_username.text()
-        users = self.data.get("user").search(username).get("users")
-
-        for user in users:
-            username = QtWidgets.QLabel()
-            username.setGeometry(QtCore.QRect(10, 10, 141, 16))
-            username.setObjectName("username")
-            username_text = QtWidgets.QLabel()
-            username_text.setGeometry(QtCore.QRect(10, 40, 141, 16))
-            username_text.setObjectName("username_edit")
-            seeProfile_search = QtWidgets.QPushButton()
-            seeProfile_search.setGeometry(QtCore.QRect(350, 20, 91, 23))
-            seeProfile_search.setObjectName("seeProfile_search")
-            ConnectButton = QtWidgets.QPushButton()
-            ConnectButton.setGeometry(QtCore.QRect(450, 20, 89, 25))
-            self.verticalLayout.addWidget(username)
-            self.verticalLayout.addWidget(username_text)
-            self.verticalLayout.addWidget(seeProfile_search)
-            self.verticalLayout.addWidget(ConnectButton)
-            ConnectButton.setObjectName("ConnectButton")
-            username_text.setText(f'New Connection : {user.username}')
-            seeProfile_search.setText("See Profile")
-            ConnectButton.setText("Connect")
-            # TODO: complete this part
-            # seeProfile_search.clicked.connect(lambda )
-            ConnectButton.clicked.connect(lambda: self.connect(user.id))
-            # self.scrollArea_users.setWidget(frame)
-            # self.scrollArea_users.setWidget(self.scrollAreaWidgetContents)
-
-    def find_invitations(self):
-        _filter = {
-            'user_invited_id': self.data.get("user").id,
-            'connected': False
-        }
-        connections = Connection.find(**_filter).get('connections')
-
-        for connection in connections:
-            user = User.find_via_pk(connection.user_caller_id).get('user')
-            username = QtWidgets.QLabel()
-            username.setGeometry(QtCore.QRect(10, 10, 141, 16))
-            username.setObjectName("username_inv")
-            username_text = QtWidgets.QLabel()
-            username_text.setGeometry(QtCore.QRect(10, 40, 141, 16))
-            username_text.setObjectName("username_edit_inv")
-            seeProfile_inv = QtWidgets.QPushButton()
-            seeProfile_inv.setGeometry(QtCore.QRect(350, 20, 91, 23))
-            seeProfile_inv.setObjectName("seeProfile_inv")
-            NoConnectButton = QtWidgets.QPushButton()
-            YesConnectButton = QtWidgets.QPushButton()
-            NoConnectButton.setGeometry(QtCore.QRect(450, 20, 89, 25))
-            YesConnectButton.setGeometry(QtCore.QRect(450, 20, 89, 25))
-            self.verticalLayout.addWidget(username)
-            self.verticalLayout.addWidget(username_text)
-            self.verticalLayout.addWidget(seeProfile_inv)
-            self.verticalLayout.addWidget(YesConnectButton)
-            self.verticalLayout.addWidget(NoConnectButton)
-            YesConnectButton.setObjectName("YesConnectButton")
-            NoConnectButton.setObjectName("NoConnectButton")
-            username_text.setText(f'New Invitation : {user.username}')
-            seeProfile_inv.setText("See Profile")
-            YesConnectButton.setText("Yes, Connect")
-            NoConnectButton.setText("No")
-            # TODO: complete this part
-            # seeProfile_inv.clicked.connect(lambda: self.connect(user.id))
-            YesConnectButton.clicked.connect(lambda: self.accept_or_reject_inv(connection, True))
-            NoConnectButton.clicked.connect(lambda: self.accept_or_reject_inv(connection, False))
-            # self.scrollArea_users.setWidget(frame)
-            # self.scrollArea_users.setWidget(self.scrollAreaWidgetContents)
-
-    def accept_or_reject_inv(self, connection: 'Connection', action):
-        if action:
-            res = connection.accept_request()
-            if res['status']:
-                Notification.notify(
-                    user_id=connection.user_caller_id,
-                    type_id=connection.id,
-                    type='Connection',
-                    time=datetime.datetime.now(),
-                    event='Connect accepted'
-                )
-        else:
-            res = connection.delete()
-
-    def connect(self, user):
-        # TODO: check if user send connect request before
-        kwargs = {
-            'user_caller_id': self.data.get('user').id,
-            'user_invited_id': user
-        }
-        res = Connection.connect_request(**kwargs)
-        if res.get('status'):
-            res = Connection.find(**kwargs)
-            if res.get('status'):
-                Notification.notify(user_id=user,
-                                    type_id=res.get('connections')[-1].id,
-                                    type='Connection',
-                                    time=datetime.datetime.now(),
-                                    )
 
 
-# if __name__ == "__main__":
-#     import sys
-#     app = QtWidgets.QApplication(sys.argv)
-#     NewConnection = QtWidgets.QMainWindow()
-#     ui = Ui_NewConnection()
-#     ui.setupUi(NewConnection)
-#     NewConnection.show()
-#     sys.exit(app.exec_())
 
-ui = Ui_NewConnection()
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    NewConnection = QtWidgets.QMainWindow()
+    ui = Ui_NewConnection()
+    ui.setupUi(NewConnection)
+    NewConnection.show()
+    sys.exit(app.exec_())
---
+++ ui/seePost.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'SeePost.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(600, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.frame)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.label_3 = QtWidgets.QLabel(self.frame)
+        self.label_3.setObjectName("label_3")
+        self.verticalLayout_2.addWidget(self.label_3)
+        self.UserName = QtWidgets.QLabel(self.frame)
+        self.UserName.setObjectName("UserName")
+        self.verticalLayout_2.addWidget(self.UserName)
+        self.SeeProfile = QtWidgets.QPushButton(self.frame)
+        self.SeeProfile.setObjectName("SeeProfile")
+        self.verticalLayout_2.addWidget(self.SeeProfile)
+        self.textBrowserOfPost = QtWidgets.QTextBrowser(self.frame)
+        self.textBrowserOfPost.setObjectName("textBrowserOfPost")
+        self.verticalLayout_2.addWidget(self.textBrowserOfPost)
+        self.numberComment = QtWidgets.QLabel(self.frame)
+        self.numberComment.setObjectName("numberComment")
+        self.verticalLayout_2.addWidget(self.numberComment)
+        self.CommentButton = QtWidgets.QPushButton(self.frame)
+        self.CommentButton.setObjectName("CommentButton")
+        self.verticalLayout_2.addWidget(self.CommentButton)
+        self.ShareButton = QtWidgets.QPushButton(self.frame)
+        self.ShareButton.setObjectName("ShareButton")
+        self.verticalLayout_2.addWidget(self.ShareButton)
+        self.LikeButton = QtWidgets.QCheckBox(self.frame)
+        self.LikeButton.setObjectName("LikeButton")
+        self.verticalLayout_2.addWidget(self.LikeButton)
+        self.Number_of_like = QtWidgets.QLabel(self.frame)
+        self.Number_of_like.setObjectName("Number_of_like")
+        self.verticalLayout_2.addWidget(self.Number_of_like)
+        self.ShareFrom_2 = QtWidgets.QLabel(self.frame)
+        self.ShareFrom_2.setObjectName("ShareFrom_2")
+        self.verticalLayout_2.addWidget(self.ShareFrom_2)
+        self.ShareFrom = QtWidgets.QLabel(self.frame)
+        self.ShareFrom.setText("")
+        self.ShareFrom.setObjectName("ShareFrom")
+        self.verticalLayout_2.addWidget(self.ShareFrom)
+        self.verticalLayout.addWidget(self.frame)
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.verticalLayout.addWidget(self.pushButton_2)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.verticalLayout.addWidget(self.pushButton_3)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 600, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label_3.setText(_translate("MainWindow", "from :"))
+        self.UserName.setText(_translate("MainWindow", "User name "))
+        self.SeeProfile.setText(_translate("MainWindow", "see profile"))
+        self.numberComment.setText(_translate("MainWindow", "number of comment"))
+        self.CommentButton.setText(_translate("MainWindow", "Comment"))
+        self.ShareButton.setText(_translate("MainWindow", "share"))
+        self.LikeButton.setText(_translate("MainWindow", "like"))
+        self.Number_of_like.setText(_translate("MainWindow", "number of like"))
+        self.ShareFrom_2.setText(_translate("MainWindow", "Share from:"))
+        self.pushButton_2.setText(_translate("MainWindow", "Next"))
+        self.pushButton.setText(_translate("MainWindow", "Back"))
+        self.pushButton_3.setText(_translate("MainWindow", "Home"))
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
